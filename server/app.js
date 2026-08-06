bf4ac470df3292ab3a26949177a420e2480f401c
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-06 16:44:17 +0530

    Today complete folder Structuse and work on Authentication

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js"
+
+
+const app = express();
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+app.use("/api/auth", authRoutes)
+
+
+
+//Test Route
+app.get("/", (req, res)=>{
+    res.json({
+        success: true,
+        message: "Shoe Store API is running",
+    })
+})
+
+
+
+
+
+export default app;
---
+++ server/controllers/authController.js
@@ -0,0 +1,116 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //Validation
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    //check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exist",
+      });
+    }
+
+    // generate hashedPassword
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+
+export const login = async (req, res)=>{
+    try{
+        const { email, password } = req.body;
+
+        //validation
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All field are required",
+            });
+        }
+
+        //Find user
+        const user = await User.findOne({
+            email
+        });
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials",
+            })
+        }
+
+        const token = jwt.sign(
+            {id: user._id,},
+            process.env.JWT_SECRET,
+            { expiresIn: "7d",}
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            }
+        })
+    } catch(error){
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+            
+        });
+    }
+}
